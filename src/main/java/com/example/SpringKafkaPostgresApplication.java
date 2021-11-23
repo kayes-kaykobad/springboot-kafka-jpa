97297659660ceffc041888954fe839ee6a548a13
unknown <audwit100@gmail.com>
2021-11-23 16:58:29 -0600

    Successfully saved job to database via command line runner

---
+++ src/main/java/com/example/SpringKafkaPostgresApplication.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.entity.Job;
+import com.example.repository.JobRepository;
+
+
+
+//import com.example.entity.Student;
+
+@SpringBootApplication
+public class SpringKafkaPostgresApplication implements CommandLineRunner {
+
+	@Autowired
+	JobRepository jobRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaPostgresApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		jobRepository.save(new Job("Started"));
+
+		jobRepository.findById(1l).ifPresent(x -> System.out.println(x.getStatus()));
+
+	}
+
+}
---
+++ src/main/java/com/example/entity/Job.java
@@ -0,0 +1,37 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Job {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;//SequenceStyleGenerator should be used as 
+	private String status;
+
+	public Job() {
+	}
+
+	public Job(String status) {
+		this.status = status;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
