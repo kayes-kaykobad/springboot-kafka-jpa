bcede5067892ca5430e32ffa04abfdef0531a7fc
unknown <audwit100@gmail.com>
2021-11-23 14:42:04 -0600

    Starting point for my project

---
+++ src/main/java/com/example/springkafkapostgres/SpringKafkaPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springkafkapostgres/SpringKafkaPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkapostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
